1b9e32dfd1758e62fde271c587f1d526e18a802b
Nick Stetich <nick@stetich.com>
2014-01-20 17:41:25 -0800

    Remove sample test, add test for Card

---
+++ src/test/groovy/com/rallydev/pokerhand/CardSpec.groovy
@@ -0,0 +1,19 @@
+package com.rallydev.pokerhand
+
+import spock.lang.Specification
+
+class CardSpec extends Specification {
+    def "card should have a rank and a suit"() {
+        given: 
+        def card = new Card(rank:rank, suit:suit)
+        
+        expect:
+        card != null
+        card.rank == rank
+        card.suit == suit
+
+        where:
+        rank     | suit
+        Rank.ACE | Suit.HEARTS
+   }
+}
