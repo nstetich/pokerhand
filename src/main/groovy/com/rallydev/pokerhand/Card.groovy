34792146849b6c5dcc6cec07407d6d756f6d5e68
Nick Stetich <nick@stetich.com>
2014-01-20 22:31:45 -0800

    Add parsing of text to rank

--- src/main/groovy/com/rallydev/pokerhand/Card.groovy
+++ src/main/groovy/com/rallydev/pokerhand/Card.groovy
@@ -1,8 +1,29 @@
 package com.rallydev.pokerhand
 
 enum Rank {
-    TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN,
-    JACK, QUEEN, KING, ACE
+    TWO('2'), 
+    THREE('3'), 
+    FOUR('4'), 
+    FIVE('5'), 
+    SIX('6'), 
+    SEVEN('7'), 
+    EIGHT('8'), 
+    NINE('9'), 
+    TEN('10'),
+    JACK('J'), 
+    QUEEN('Q'), 
+    KING('K'), 
+    ACE('A');
+
+    public final String text
+
+    Rank(String text) {
+        this.text = text
+    }
+    
+    public static Rank parse(String str) {
+       return values().find { it.text == str }
+    }
 }
 
 enum Suit {
--- src/test/groovy/com/rallydev/pokerhand/CardSpec.groovy
+++ src/test/groovy/com/rallydev/pokerhand/CardSpec.groovy
@@ -48,4 +48,26 @@ class CardSpec extends Specification {
         where:
         otherRank << (Rank.values() - Rank.ACE)
     }
+
+    def "rank should support parsing from string representation"() {
+        expect:
+        rank == Rank.parse(str)
+
+        where:
+        rank       | str
+        Rank.TWO   | '2'
+        Rank.THREE | '3'
+        Rank.FOUR  | '4'
+        Rank.FIVE  | '5'
+        Rank.SIX   | '6'
+        Rank.SEVEN | '7'
+        Rank.EIGHT | '8'
+        Rank.NINE  | '9'
+        Rank.TEN   | '10'
+        Rank.JACK  | 'J'
+        Rank.QUEEN | 'Q'
+        Rank.KING  | 'K'
+        Rank.ACE   | 'A'
+    }
+
 }
