9f93312420ca7031d8025f28a6ad8b35ad8f388b
Nick Stetich <nick@stetich.com>
2014-01-23 08:31:28 -0800

    Better support for piped stdin

--- src/main/groovy/com/rallydev/pokerhand/Application.groovy
+++ src/main/groovy/com/rallydev/pokerhand/Application.groovy
@@ -2,11 +2,14 @@ package com.rallydev.pokerhand
 
 class Application {
     public static void main(String[] args) {
-        println "Enter a hand, each card formatted [RANK][suit]"
-        println "e.g Js = Jack of Spades, 10d = 10 of Diamonds"
-        println "Type an empty line to quit."
+        if (System.console()) {
+            println "Enter a hand, each card formatted [RANK][suit]"
+            println "e.g Js = Jack of Spades, 10d = 10 of Diamonds"
+            println "Type an empty line to quit."
+        }
         String line
-        while (line = System.console().readLine()) { 
+        def input = System.console() ?: System.in.newReader()
+        while (line = input.readLine()) { 
             try {
                 if (line.empty) { 
                     break 
@@ -15,7 +18,9 @@ class Application {
             } catch (IllegalArgumentException e) {
                 println "${e.class.name}: ${e.message}"
             }
+        } 
+        if (System.console()) {
+            println "Goodbye!"
         }
-        println "Goodbye!"
     }
 }
